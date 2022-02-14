ec45774c579d8961be4cd2f674156b0929733fe0
Stranger <2633565580@qq.com>
2022-02-14 14:33:11 +0800

    add TimeFormat

--- CheckMe.js
+++ CheckMe.js
@@ -1,9 +1,9 @@
 function checkme(e){
     if(e.group_id != NIL.CONFIG.GROUP_MAIN)return;
     if(e.raw_message == '我的统计'){
-        if(NIL.XDB.wl_exsis(e.sender.user_id)){
+        if(NIL.XDB.wl_exsits(e.sender.user_id)){
             var pl = NIL.XDB.get_player(e.sender.user_id);
-            var str = `[${pl.xboxid}]\n加入次数：${pl.count.join}\n游玩时间：${pl.count.duration}`;
+            var str = `玩家名: ${pl.xboxid}\n进服次数: ${pl.count.join}\n游玩时间: ${timeFormat(pl.count.duration)} 小时`;
             e.reply(str);
         }else{
             e.reply('你还没绑定xboxid!',true);
@@ -11,7 +11,13 @@ function checkme(e){
     }
 }
 
-
+function timeFormat(dur){
+    if (dur!==0){
+        let hour=3600*10000;
+        return (dur/hour).toFixed(2);
+    }
+    return 0;
+}
 
 function onStart(){
     NIL.FUNC.PLUGINS.GROUP.push(checkme);
